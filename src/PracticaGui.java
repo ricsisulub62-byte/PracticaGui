4d705c45fe4613abc8e95ea9d7df7be83bff86a1
Ricsi <ricsisulub62@gmail.com>
2026-03-26 10:30:48 -0600

    Agregue un boton hola desde el master

---
+++ src/PracticaGui.java
@@ -0,0 +1,55 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class PracticaGui extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					PracticaGui frame = new PracticaGui();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public PracticaGui() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnMaster = new JButton("Botón Master");
+		btnMaster.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(btnMaster, "Hola desde el master");
+			}
+			
+		});
+		btnMaster.setBounds(65, 55, 131, 23);
+		contentPane.add(btnMaster);
+
+	}
+
+}
